c21772a2803cbd9bb98558431da20d3a90620107
Evan McCullough <webmaster@evanamccullough.com>
2014-01-15 09:56:18 -0500

    finished user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 RatedateRuby::Application.routes.draw do
   
   root 'static_pages#index'
+
+  
   match '/terms', to: 'static_pages#terms', via: 'get'
   match '/privacy', to: 'static_pages#privacy', via: 'get'
+
+
+  resources :users
 
 end
