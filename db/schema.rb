340b9f8fd3a10aec1c4f92de0c49d3f4d96c1714
Evan McCullough <webmaster@evanamccullough.com>
2014-01-15 15:17:15 -0500

    removed authorization

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140115183530) do
-
-  create_table "authorizations", force: true do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140115162825) do
 
   create_table "users", force: true do |t|
     t.string   "fname"
