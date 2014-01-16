157b4b26d2feb69875b7f1d8badc63b7c2535d03
Evan McCullough <webmaster@evanamccullough.com>
2014-01-15 20:43:19 -0500

    fixes to facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ RatedateRuby::Application.routes.draw do
   match '/privacy', to: 'static_pages#privacy', via: 'get'
   match '/aboutus', to: 'static_pages#aboutus', via: 'get'
 
-  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/:provider', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 end
