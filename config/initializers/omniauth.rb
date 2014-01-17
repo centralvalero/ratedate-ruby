720efcbf504ebff7f59e8bf066740b51b71e2dc2
Evan McCullough <webmaster@evanamccullough.com>
2014-01-17 09:49:06 -0500

    update omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
-
+ 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['333505546788462'], ENV['e2124f1b88ccb4e4274544bd3130f9df']
 end
