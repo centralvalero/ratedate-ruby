054937f204fa6330e9a694a081ca530229c4f184
Evan McCullough <webmaster@evanamccullough.com>
2014-01-17 09:50:35 -0500

    added twitter to initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ OmniAuth.config.logger = Rails.logger
  
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['333505546788462'], ENV['e2124f1b88ccb4e4274544bd3130f9df']
+  #provider :twitter, ENV['SOME-INFO-HERE'], ENV['SOME-INFO-HERE']
 end
