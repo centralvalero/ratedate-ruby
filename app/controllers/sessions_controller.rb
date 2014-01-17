4479dbe88a6d516d78ea0b21a5edfc0dc2779b02
Evan McCullough <webmaster@evanamccullough.com>
2014-01-17 09:46:53 -0500

    update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,18 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
+    auth = request.env["omniauth.auth"]
+    user = User.where(:provider =&gt; auth['provider'], :uid =&gt; auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    reset_session
     session[:user_id] = user.id
     redirect_to root_url
   end
-
+ 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_url
+    reset_session
+    redirect_to root_url, :notice =&gt; 'Signed out!'
+  end
+ 
+  def failure
+    redirect_to root_url, :alert =&gt; "Authentication error: #{params[:message].humanize}"
   end
 end
