a2b9f559561707ffb442149129feaf41a0f00d13
Evan McCullough <webmaster@evanamccullough.com>
2014-01-18 01:40:24 -0500

    updates to routes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
  
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['333505546788462'], ENV['e2124f1b88ccb4e4274544bd3130f9df']
-  #provider :twitter, ENV['SOME-INFO-HERE'], ENV['SOME-INFO-HERE']
+  provider :facebook, '333505546788462', 'e2124f1b88ccb4e4274544bd3130f9df'
+  provider :twitter, '9uwdxjJD2lR5lqZWJspSWw', 'N7WC66nwOJTi6mQylAp2AOYOFNPzmPgDUbeh0NET6m4'
 end
