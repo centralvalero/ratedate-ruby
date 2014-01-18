af9fb459f483602d830c4b309568995a12d842de
Evan McCullough <webmaster@evanamccullough.com>
2014-01-18 02:18:11 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ RatedateRuby::Application.routes.draw do
   match '/aboutus', to: 'static_pages#aboutus', via: 'get'
 
 
-  get '/auth/:provider/callback'  => 'sessions#create'
-  post '/signin'                  => 'sessions#new',        :as => :signin
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
-  get '/auth/failure'             => 'sessions#failure'
+  match '/auth/:provider/callback', to: 'sessions#create', via: "get"
+  match '/signin', to: 'sessions#new', via: 'post'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+  match '/auth/failure', to: 'sessions#failure', via: "get"
 end
