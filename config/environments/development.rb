5c0691f5c7633e37af4bc5620238196d8585cfc4
Evan McCullough <webmaster@evanamccullough.com>
2014-01-19 16:37:50 -0500

    Revert 13ef3a5..fded25b
    This rolls back to commit 13ef3a5260df710bb6d6af268f1f85ef56d34d3f.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,9 +9,6 @@ RatedateRuby::Application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  #for devise
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 RatedateRuby::Application.routes.draw do
 
 
-  devise_for :users, :path => "accounts", :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations" }
   root 'static_pages#index'
+  resources :sessions, only: [:create, :destroy]
 
   
   match '/terms', to: 'static_pages#terms', via: 'get'
   match '/privacy', to: 'static_pages#privacy', via: 'get'
   match '/aboutus', to: 'static_pages#aboutus', via: 'get'
+
+
+  match '/auth/:provider/callback', to: 'sessions#create', via: "get"
+  match '/signin', to: 'sessions#new', via: 'post'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+  match '/auth/failure', to: 'sessions#failure', via: "get"
 end
