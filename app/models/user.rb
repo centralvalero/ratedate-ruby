129cb83c29a2a5efc6dbf51c7654569517eb68e7
Evan McCullough <webmaster@evanamccullough.com>
2014-01-26 17:41:06 -0500

    updates to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :twitter]
 
  	def self.find_for_facebook_oauth(auth)
-		user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    	unless 
-    		user = User.create(name:auth.extra.raw_info.name, provider:auth.provider, uid:auth.uid, email:auth.info.email, password:Devise.friendly_token[0,20])
-    	end
+	  user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    	unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                           provider:auth.provider,
+                           uid:auth.uid,
+                           email:auth.info.email,
+                           password:Devise.friendly_token[0,20])
+    end
+    user
 	end
 
 	def self.find_for_twitter_oauth(auth)
