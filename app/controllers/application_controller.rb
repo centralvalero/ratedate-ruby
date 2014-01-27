8912fd97a11104802fd17f43a1c44069284ef837
Evan McCullough <webmaster@evanamccullough.com>
2014-01-27 11:14:36 -0500

    updated index page css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
  
   helper :all # include all helpers, all the time
-include ActionView::Helpers::NumberHelper
+  protected
+
+	def configure_permitted_parameters
+	    devise_parameter_sanitizer.for(:sign_up) << :name
+	end
 end
